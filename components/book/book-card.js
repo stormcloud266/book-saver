d018b819b48c3ce7f2367a1be96fcc289af32900
Tawnee <allprt89@gmail.com>
2022-04-12 09:52:04 -0600

    add placeholder image. fix aspect ratio

--- components/book/book-card.js
+++ components/book/book-card.js
@@ -14,6 +14,9 @@ const BookCard = ({
 	const book = { title, first_publish_year, cover_i, id_goodreads, book_id }
 	const { favorites, setFavorites } = useFavorites()
 	const [isFavorite, setIsFavorite] = useState(false)
+	const imageUrl = cover_i
+		? `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`
+		: 'http://placehold.jp/3d4070/ffffff/200x300.jpg?text=Image%20Not%20Available'
 
 	useEffect(() => {
 		setIsFavorite(!!favorites.find((item) => item.book_id === book.book_id))
@@ -62,19 +65,27 @@ const BookCard = ({
 			<p>
 				<small>{first_publish_year}</small>
 			</p>
-			{/* <Image
-				layout='intrinsic'
-				width={200}
-				height={300}
-				src={`https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`}
-			/> */}
+
+			<div
+				style={{
+					height: '300px',
+					maxWidth: '180px',
+					position: 'relative',
+					backgroundColor: 'black',
+					overflow: 'hidden',
+					borderRadius: 4,
+				}}
+			>
+				<Image height={300} width={180} objectFit='contain' src={imageUrl} />
+			</div>
 			{/* <a
 						href={`https://www.goodreads.com/book/show/${id_goodreads[0]}`}
 						target='_blank'
 						rel='noopener noreferrer'
 					>
 						Open In Goodreads
-					</a> */}
+					</a> 
+				*/}
 		</div>
 	)
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	images: {
-		domains: ['covers.openlibrary.org'],
+		domains: ['covers.openlibrary.org', 'placehold.jp'],
 	},
 }
