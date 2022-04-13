fc8ee2a527d11044ee4bc7730f16cd82278db819
Tawnee <allprt89@gmail.com>
2022-04-12 21:11:34 -0600

    add placeholder. use search

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import { useFavorites } from '../context/favorites-context'
 import Search from '../components/search/search'
 import BookCard from '../components/book/book-card'
 import Grid from '../components/grid/grid'
+import Placeholder from '../components/placeholder/placeholder'
 
 const test = require('../test.json')
 
 function HomePage() {
-	// return <StartingPageContent />;
 	const [books, setBooks] = useState(null)
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState(false)
@@ -27,56 +27,24 @@ function HomePage() {
 
 			{loading && <h2>loading...</h2>}
 			{error && <h2>error...</h2>}
+			{books?.numFound === 0 && <h2>no results</h2>}
+			{(!books || books?.numFound === 0) && <Placeholder />}
 
-			<Grid>
-				{test.docs.map((book) => (
-					<BookCard
-						title={book.title}
-						first_publish_year={book.first_publish_year}
-						cover_i={book.cover_i}
-						id_goodreads={book.id_goodreads}
-						book_id={book.key}
-						key={book.key}
-						isLoggedIn={isLoggedIn && isLoaded}
-					/>
-				))}
-			</Grid>
-
-			{/* {books && !error && (
-				<div>
-					{books.docs.length === 0 ? (
-						<h2>no results</h2>
-					) : (
-						<>
-							{books.docs.map(
-								({ title, first_publish_year, cover_i, id_goodreads }) => (
-									<div key={cover_i}>
-										<p>
-											<b>{title}</b>
-										</p>
-										<p>
-											<small>{first_publish_year}</small>
-										</p>
-										<Image
-											layout='intrinsic'
-											width={200}
-											height={300}
-											src={`https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`}
-										/>
-										<a
-											href={`https://www.goodreads.com/book/show/${id_goodreads[0]}`}
-											target='_blank'
-											rel='noopener noreferrer'
-										>
-											Open In Goodreads
-										</a>
-									</div>
-								)
-							)}
-						</>
-					)}
-				</div>
-			)} */}
+			{books && !error && (
+				<Grid>
+					{books.docs.map((book) => (
+						<BookCard
+							title={book.title}
+							first_publish_year={book.first_publish_year}
+							cover_i={book.cover_i}
+							id_goodreads={book.id_goodreads}
+							book_id={book.key}
+							key={book.key}
+							isLoggedIn={isLoggedIn && isLoaded}
+						/>
+					))}
+				</Grid>
+			)}
 		</div>
 	)
 }
